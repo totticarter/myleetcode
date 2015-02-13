3ea7c0a78c1b2e8718d2e585dd61d523ce420e50
liyong <liyong03082218@aliyun.com>
2015-02-13 09:41:47 -0500

    first

--- LongestSubstringWithoutRepeatingCharacters.cc
+++ LongestSubstringWithoutRepeatingCharacters.cc
@@ -1,8 +1,45 @@
 #include<iostream>
+#include<string.h>
 
 using namespace std;
+bool match_any_one_char(char* longest, int len, char c){
 
+	for(int i = 0; i < len; i++){
+
+		if(longest[i] == c){
+		
+			return true;
+		}
+	}
+	return false;
+
+}
+char*  find_longest(char* a, int len){
+
+	char longest[len];
+	for(int i = 0; i < len; i++){
+
+		longest[0] = a[i];
+		int pos = 0;
+		for(int j = i+1; j < len; j++){
+
+			if(match_any_one_char(longest, pos, a[j])){
+				
+				break;		
+			}
+			else{
+				
+				longest[++pos] = a[j];
+			}
+		}
+	}
+
+	cout << longest << endl;
+}		
 int main(){
 
-return 0;
+	char a[] = "abcdabcabc";
+	char* longest = find_longest(a, strlen(a));		
+
+	return 0;
 }
