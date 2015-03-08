8ffc13f6348274b6caa5e3619490456679fb6ceb
liyong <totticarter@163.com>
2015-03-08 17:02:52 +0800

    accepted

--- PascalTriangle.cc
+++ PascalTriangle.cc
@@ -10,57 +10,83 @@ public:
     vector<int> getRow(int rowIndex) {
 
         vector<int> pre_rows(rowIndex+1);
-        vector<int> cur_rows(rowIndex+1);
-        cout << "cur_rows size is: " << cur_rows.size() << endl;
-        for(int cur_row = 0; cur_row < rowIndex+1; cur_row++){
+        vector<int> return_rows(rowIndex+1);
+        cout << "cur_rows size is: " << return_rows.size() << endl;
 
-            if(cur_row == 0){
+		if(rowIndex == 0){
 
-                 cur_rows[0] = 1;
-                 if(rowIndex == 0){
+			 return_rows[0] = 1;
+			 return return_rows;
+		}else if(rowIndex == 1){
 
-                    return cur_rows;    
-                 }else{
+			return_rows[0] = 1;
+			return_rows[1] = 1;
+			return return_rows;
+		}else{
 
-                	 cur_rows.assign(rowIndex+1, 0);
-                 }
+			pre_rows = getRow(rowIndex - 1);
+			return_rows[0] = 1;
+			for(int i = 1; i < rowIndex; i++){
 
-            }else if(cur_row == 1){
+				return_rows[i] = pre_rows[i-1] + pre_rows[i];
+			}
+			return_rows[rowIndex] = 1;
+		}
 
-                cur_rows[0] = 1;
-                cur_rows[1] = 1;
-                pre_rows.assign(cur_rows.begin(), cur_rows.end());
-                if(rowIndex == 1){
 
-                    return cur_rows;    
-                }else{
 
-                	cur_rows.assign(rowIndex+1, 0);
-                }
-            }else{
 
-                for(int i = 0; i < cur_row; i++){
-
-                    if(i == 0){
-
-                        cur_rows[0] = 1;
-                        cur_rows[cur_row] = 1;
-                    }else{
-
-                        cur_rows[i] = pre_rows[i - 1] + pre_rows[i];
-                    }
-                }
-                cout << "cur_rows size is: " << cur_rows.size() << endl;
-                return cur_rows;
-            }
-        }
+//        for(int cur_row = 0; cur_row < rowIndex+1; cur_row++){
+//
+//            if(cur_row == 0){
+//
+//                 cur_rows[0] = 1;
+//                 if(rowIndex == 0){
+//
+//                    return cur_rows;
+//                 }else{
+//
+//                	 cur_rows.assign(rowIndex+1, 0);
+//                 }
+//
+//            }else if(cur_row == 1){
+//
+//                cur_rows[0] = 1;
+//                cur_rows[1] = 1;
+//                pre_rows.assign(cur_rows.begin(), cur_rows.end());
+//                if(rowIndex == 1){
+//
+//                    return cur_rows;
+//                }else{
+//
+//                	cur_rows.assign(rowIndex+1, 0);
+//                }
+//            }else{
+//
+//            	pre_rows = getRow();
+//                for(int i = 0; i < cur_row; i++){
+//
+//                	  pre_rows = getRow()
+////                    if(i == 0){
+////
+////                        cur_rows[0] = 1;
+////                        cur_rows[cur_row] = 1;
+////                    }else{
+////
+////                        cur_rows[i] = pre_rows[i - 1] + pre_rows[i];
+////                    }
+//                }
+//                cout << "cur_rows size is: " << cur_rows.size() << endl;
+//                return cur_rows;
+//            }
+//        }
     }
 };
 
 int main(){
 
 	Solution s;
-	vector<int> v = s.getRow(3);
+	vector<int> v = s.getRow(4);
 	cout << "v size is: " << v.size() << endl;
 	vector<int>::iterator iter = v.begin();
 	for(; iter != v.end(); iter++){
