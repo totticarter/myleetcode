af882fe78105d3ab7fd83f58ad7bce6ddc0ea198
liyong <totticarter@163.com>
2015-03-10 22:55:48 +0800

    add

---
+++ ReverseBits.cc
@@ -0,0 +1,18 @@
+//For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+
+#include<iostream>
+
+
+using namespace std;
+
+uint32_t reverseBits(uint32_t n) {
+
+
+
+}
+
+int main(){
+
+
+}
