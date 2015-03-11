c4730c30da80a0df4c36c5a16ba69d11c8f71e3b
liyong <young.li@huawei.com>
2015-03-11 11:11:12 +0800

    fix

--- MergeSortedArray.cc
+++ MergeSortedArray.cc
@@ -1,4 +1,7 @@
+//merge from tail, will not override the element in A
+
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 
@@ -12,15 +15,37 @@ int find_equal_or_less(int bval, int A[], int m){
 			return i;
 	}
 }
-void insert_into_a(int bval, int insert_pos)
+void insert_into_a(int bval, int insert_pos);
 void merge(int A[], int m, int B[], int n){
 
-	int idx = 0;
-	while(idx++ < n){
+	//int idx = 0;
+	//while(idx++ < n){
 
-		int bval = B[idx];
-		int insert_pos = find_equal_or_less(bval, A, m);	
-		insert_into_a(bval, insert_pos, int A[], int m);
+	//	int bval = B[idx];
+	//	int insert_pos = find_equal_or_less(bval, A, m);	
+	//	insert_into_a(bval, insert_pos, int A[], int m);
+	//}
+	
+	int newArraySize = m+n;
+	
+	while(m > 0 && n > 0){
+
+		if(A[m] > B[n]){
+
+			A[newArraySize--] = A[m--];
+		}else if(A[m] == B[n]){
+
+			A[newArraySize--] = A[m--];
+			A[newArraySize--] = B[n--];
+		}else{
+			
+			A[newArraySize--] = B[n--];
+		}
+	}
+	
+	if(n != 0){
+
+		memcpy(A, B, n);
 	}
 
 }
